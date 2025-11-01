5bdda7d08831619f8d85bec94caedc210a418a10
GTK-kala <gtkkala1110@gmail.com>
2025-11-01 12:19:43 +0300

    continued on buliding the user page

--- FrontEnd-Section/src/components/Users/User.jsx
+++ FrontEnd-Section/src/components/Users/User.jsx
@@ -1,11 +1,58 @@
+import { useEffect, useState } from "react";
+import { FaUsers, FaCalendarAlt, FaEnvelope } from "react-icons/fa";
 import "./User.css";
 
 const User = () => {
-  return (
-    <div className='user'>
-        <h1>User Data</h1>
-    </div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default User
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/api/auth/register/all");
+        const data = await res.json();
+        const Data = data.result
+        setUsers(Data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="users-page">
+      <div className="users-header">
+        <FaUsers className="icon" />
+        <h2>All Users</h2>
+      </div>
+
+      <div className="users-grid">
+        {users.length === 0 ? (
+          <p className="no-users">No users found 👥</p>
+        ) : (
+          users.map((user) => (
+            <div key={user.id} className="user-card">
+              <div className="user-header">
+                <h3>{user.name}</h3>
+              </div>
+              <div className="user-body">
+                <p>
+                  <FaEnvelope className="icon-small" /> {user.email}
+                </p>
+              </div>
+              <div className="user-footer">
+                <FaCalendarAlt className="icon-small" />
+                <small>
+                  Joined:{" "}
+                  {new Date(user.created_at).toLocaleDateString("en-GB")}
+                </small>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default User;
