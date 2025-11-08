66d9b217534800e8288609b12a659c71aa83e828
GTK-kala <gtkkala1110@gmail.com>
2025-11-08 09:58:27 +0300

    adding the toast container only on app.jsx file

--- FrontEnd-Section/src/pages/EditUsers.jsx
+++ FrontEnd-Section/src/pages/EditUsers.jsx
@@ -1,39 +1,9 @@
 import EditUser from "../components/Accounts/EditUser/EditUser";
-import { Toaster } from "react-hot-toast";
 
 const EditUsers = () => {
   return (
     <>
       <EditUser />
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          duration: 1000,
-          removeDelay: 1000,
-          style: {
-            background: "rgba(30, 41, 59, 0.8)", // glassy dark-blue background
-            color: "#fff",
-            borderRadius: "12px",
-            padding: "12px 16px",
-            fontSize: "0.95rem",
-            backdropFilter: "blur(10px)",
-            border: "1px solid rgba(255,255,255,0.08)",
-          },
-          success: {
-            duration: 2000,
-            iconTheme: {
-              primary: "#38bdf8",
-              secondary: "#0f172a",
-            },
-          },
-          error: {
-            iconTheme: {
-              primary: "#ef4444",
-              secondary: "#0f172a",
-            },
-          },
-        }}
-      />
     </>
   );
 };
